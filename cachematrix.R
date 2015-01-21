23d27bf5114217c19e07fa692c362513a19e35ce
Martin Cote <martin.cote@gmail.com>
2015-01-20 19:39:54 -0500

    Completed assignment
    Completed assignment for both makeCacheMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,109 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+### makeCacheMatrix list
+## Description
+# makeCacheMatrix list
+#
+## Usage
+# makeCacheMatrix(x)
+# 
+## Arguments
+# x   matrix object to be inverse.
+#
+## Details
+# This function creates a special "matrix" object that can cache its inverse.
+#
+## Sources
+# Refer to: https://class.coursera.org/rprog-010/human_grading/view/courses/973491/assessments/3/submissions
+# Refer to cacheSolve()
+#
+## References
+# Refer to Lexical Scoping (i.e. scoping rules) of R
+# 
+## Examples
+# tst_matrix4x4 <- matrix(c(1,1,1,1,1,2,1,4,1,1,1,2,1,2,0,3), nrow=4, ncol=4)
+# cachematrix4x4 <- makeCacheMatrix(tst_matrix4x4)
+# cacheSolve(cachematrix4x4) # Call once (without cache)...
+# cacheSolve(cachematrix4x4) # Call twice (with cache)...
+#
 makeCacheMatrix <- function(x = matrix()) {
   
-  inv <- NULL
+  # attributes
+  # inverseMatrix: inverse matrix data cached, filled after performed once
+  inverseMatrix <- NULL
+  
+  # getter/setter
+  # setter for original matrix data
   set <- function(y) {
     x <<- y
-    inv <<- NULL
+    # assuming data has changed regardless, re-initializing the inversed matrix
+    inverseMatrix <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
-  list(set = set, get = get,
+  # getter for original matrix data
+  get <- function() {
+    x
+  }
+  # setter for the inverse matrix
+  setinverse <- function(inverseMtrx) {
+    inverseMatrix <<- inverseMtrx
+  }
+  # getter for the inverse matrix
+  getinverse <- function() {
+    inverseMatrix
+  }
+  
+  # return list of methods (i.e. exposing the methods)
+  list(set = set,
+       get = get,
        setinverse = setinverse,
        getinverse = getinverse)
-  
 }
 
-
-## Write a short comment describing this function
-
+### Solve a System of Equations
+## Description
+# Return a matrix that is the inverse of 'x'
+#
+## Usage
+# cacheSolve(x, ...)
+# 
+## Arguments
+# x   a square invertible matrix "augmented" object (see makeCacheMatrix function)
+#     Note: It is assumed that the matrix supplied is always invertible.
+# ... further arguments passed to or from other methods.
+#
+## Details
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+# If the inverse has already been calculated (and the matrix has not changed), then the cachesolve
+# retrieve the inverse from the cache.
+#
+## Sources
+# Refer to: https://class.coursera.org/rprog-010/human_grading/view/courses/973491/assessments/3/submissions
+# Refer to makeCacheMatrix
+# 
+## References
+# Refer to R:Solve a System of Equations, part of 'base' package.
+# 
+## Examples
+# tst_matrix4x4 <- matrix(c(1,1,1,1,1,2,1,4,1,1,1,2,1,2,0,3), nrow=4, ncol=4)
+# cachematrix4x4 <- makeCacheMatrix(tst_matrix4x4)
+# cacheSolve(cachematrix4x4) # Call once (without cache)...
+# cacheSolve(cachematrix4x4) # Call twice (with cache)...
+#
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Retreive cached inverse
+  inverse_matrix <- x$getinverse()
   
-  iv <- x$getinverse()
-  if(!is.null(iv)) {
+  # if available, return it from cache
+  if(!is.null(inverse_matrix)) {
     message("getting cached data")
-    return(iv)
+    return(inverse_matrix) # stop further processing
   }
+  
+  # Otherwise:
+  # retrieve original inversible matrix
   data <- x$get()
-  iv <- solve(data, ...)
-  x$setinverse(iv)
-  iv
+  # computes inverse matrix
+  inverse_matrix <- solve(data, ...) 
+  # cache inverse matrix for further use
+  x$setinverse(inverse_matrix)
+  # return inverse matrix
+  inverse_matrix
 }
--- unittestcachematrix.R
+++ unittestcachematrix.R
@@ -3,13 +3,10 @@
 source("cachematrix.R")
 
 tst_matrixempty <- matrix()
-
 # Matrix 2x2
 tst_matrix2x2 <- matrix(1:4, nrow=2, ncol=2)
-
 # Matrix 3x3
 tst_matrix3x3 <- matrix(c(1,0,5,2,1,6,3,4,0), nrow=3, ncol=3)
-
 # Matrix 4x4
 tst_matrix4x4 <- matrix(c(1,1,1,1,1,2,1,4,1,1,1,2,1,2,0,3), nrow=4, ncol=4)
 
@@ -40,11 +37,13 @@ cacheSolve(cachematrix3x3)
 # Call third (with cache)...
 cacheSolve(cachematrix3x3)
 
-## Test changing data on 3x3 matrix...
-tst_matrix3x3 <- matrix(c(2,0,0,0,1,0,2,2,2), nrow=3, ncol=3)
-# Call cacheSolve without "notifying" the method
-cacheSolve(cachematrix3x3) # TODO: error; same matrix as before.  Are we supposed to support that scenario?
-
+# Test setter
+tst_matrix3x3bis <- matrix(c(2,3,2,1,2,1,1,1,2), nrow=3, ncol=3)
+cachematrix3x3$set(tst_matrix3x3bis)
+# Call once (without cache)...
+cacheSolve(cachematrix3x3)
+# Call twice (with cache)...
+cacheSolve(cachematrix3x3)
 
 ## Test 4x4 matrix
 cachematrix4x4 <- makeCacheMatrix(tst_matrix4x4)
@@ -54,3 +53,10 @@ cacheSolve(cachematrix4x4)
 cacheSolve(cachematrix4x4)
 # Call third (with cache)...
 cacheSolve(cachematrix4x4)
+
+## Test original R documentation "solve" examples
+hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+h8 <- hilbert(8);
+cache_h8 <- makeCacheMatrix(h8)
+sh8 <- cacheSolve(cache_h8); sh8
+sh8 <- cacheSolve(cache_h8); sh8
